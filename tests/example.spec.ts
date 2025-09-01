357fabc755b790cb488b2fb064f0f078625c9243
Stanislao Vargas <e_vargas.v@outlook.com>
2025-09-01 00:35:45 -0400

    localizando búsqueda de producto y mostrando ventana de búsqueda encontrada

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -20,7 +20,10 @@ test('get started link', async ({ page }) => {
 test('initial test', async ({page}) => {
 
   await page.goto('https://www.mercadolibre.com.co')
-
   await page.locator('input[id=\'cb1-edit\']').fill('Iphone')
+  await page.keyboard.press('Enter')
+
+  await expect(page.locator('//ol[contains(@class, \'ui-search-layout\')]')).toBeVisible()
+  await page.pause()
 
 });
