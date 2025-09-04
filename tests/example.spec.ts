f9d5976562f4ab3de2a681c6d5fb39ae012e862a
Stanislao Vargas <e_vargas.v@outlook.com>
2025-09-04 18:04:43 -0400

    sacando texto de los distintos textos buscados

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -22,8 +22,18 @@ test('initial test', async ({page}) => {
   await page.goto('https://www.mercadolibre.com.co')
   await page.locator('input[id=\'cb1-edit\']').fill('Iphone')
   await page.keyboard.press('Enter')
-
+  
   await expect(page.locator('//ol[contains(@class, \'ui-search-layout\')]')).toBeVisible()
-  await page.pause()
+  //await page.pause()
+  
+  
+  
+  const titles = await page.locator('//ol[contains(@class, \'ui-search-layout\')]//li//h2').allInnerTexts()
+  
+  console.log('The total number of result is:', titles.length)
+  for(let title of titles){
+    console.log('The titles is: ', title)
+  }
+  //await page.pause()
 
 });
