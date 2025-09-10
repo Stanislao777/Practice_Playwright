578e15f41aaa4a3f0dc9191e0d806f4cdc6c5286
Stanislao Vargas <e_vargas.v@outlook.com>
2025-09-10 12:56:20 -0400

    Haciendo uso de xxpath del css selectors

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -10,3 +10,12 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'iPhone 16e (128 Gb) - Negro - Distribuidor Autorizado' }).click();
   await page.getByRole('button', { name: 'Comprar ahora' }).click();
 });
+
+test('test locators', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/index.html');
+  await page.pause()
+  
+  //await page.locator('#name').fill('algo')
+  await page.locator('xpath=//input[@class="form"]').fill('Introduciendo texto "algo"')
+  await page.pause()
+});
