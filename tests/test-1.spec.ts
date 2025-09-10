30dcb6111bc410adbaf18916a1e49978d6fca923
Stanislao Vargas <e_vargas.v@outlook.com>
2025-09-10 00:26:35 -0400

    corrigiendo errores de record para que pase el test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -2,4 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   // Recording...
+  await page.goto('https://www.mercadolibre.com.co');
+  await page.getByRole('link', { name: 'Colombia' }).click();
+  await page.getByRole('combobox', { name: 'Ingresa lo que quieras' }).click();
+  await page.getByRole('combobox', { name: 'Ingresa lo que quieras' }).fill('Iphone');
+  await page.getByRole('combobox', { name: 'Ingresa lo que quieras' }).press('Enter');
+  await page.getByRole('link', { name: 'iPhone 16e (128 Gb) - Negro - Distribuidor Autorizado' }).click();
+  await page.getByRole('button', { name: 'Comprar ahora' }).click();
 });
