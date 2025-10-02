95c34c6fea66accc191298a5dd1f0f29e2da715d
Stanislao Vargas <e_vargas.v@outlook.com>
2025-10-01 22:45:29 -0400

    iniciando test de la tabla y pasando con éxito

--- tests/workingWithTables.spec.ts
+++ tests/workingWithTables.spec.ts
@@ -4,12 +4,21 @@ test('test web table', async ({ page }) => {
 
   await page.goto('https://cosmocode.io/automation-practice-webtable/')
 
+  const tableContainer = await page.locator("xpath=//table[@id='countries']")
+
+  const rows = await tableContainer.locator("xpath=.//tr").all()
   
+  console.log(rows.length)
+
+  for(let row of rows){
+    console.log(await row.innerText())
+  }
 
   await page.pause()
 });
 
 /*
+element container: //table[@id='countries']
 .//tr -> filas
 
 //table[@id='countries']//tr[2]//td[1] -> Check
