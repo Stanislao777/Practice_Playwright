a6ca84cde7f112437c1ff89e9aa2e7f4677bcdef
Stanislao Vargas <e_vargas.v@outlook.com>
2025-10-20 21:11:38 -0400

    Utilizando Interceptor para evitar que carguen ciertos objetos del sitio web en el test

--- tests/PageObjects/LoginPage.ts
+++ tests/PageObjects/LoginPage.ts
@@ -1,15 +1,17 @@
-import { Locator, Page } from "@playwright/test"
+import { Locator, Page, expect } from "@playwright/test"
 
 export class LoginPage{
 
     private readonly usernameTextbox: Locator
     private readonly passwordTextbox: Locator
     private readonly loginButton: Locator
+    private readonly shoppingCartIcon: Locator
 
     constructor(page: Page){
         this.usernameTextbox = page.getByRole('textbox', { name: 'Username' })
         this.passwordTextbox = page.getByRole('textbox', { name: 'Password' })
         this.loginButton = page.getByRole('button', { name: 'Login' })
+        this.shoppingCartIcon = page.locator("xpath=//a[contains(@class, 'shopping_cart_link')]")
     }
 
     async fillUsername(userName:string){
@@ -29,6 +31,10 @@ export class LoginPage{
         await this.fillUsername(userName)
         await this.fillPassword(password)
         await this.clickOnLogin()
+    }
+
+    async checkSuccessfulLogin(){
+        await expect(this.shoppingCartIcon).toBeVisible()
     }
 
 }
---
+++ tests/interceptor.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from './pageObjects/LoginPage';
+
+test('purchase an item 2', async ({ page }) => {
+    
+    await page.on("request", (req) => {
+        console.log(req.url())
+    })
+
+    await page.route(
+        "https://www.saucedemo.com/static/media/bike-light-1200x1500.37c843b0.jpg",
+        (route) => route.abort()
+    );
+    
+    await page.route(
+        "https://www.saucedemo.com/static/media/bolt-shirt-1200x1500.c2599ac5.jpg",
+        (route) => route.abort()
+    );
+
+    await page.goto("https://saucedemo.com");
+    const login = new LoginPage(page);
+    await login.loginWithCredentials("standard_user", "secret_sauce");
+    await login.checkSuccessfulLogin();
+    
+    
+    const itemsContainer = await page
+    .locator("#inventory_container .inventory_item")
+    .all();
+
+   await page.screenshot({path:'login.png', fullPage:true}) 
+})
